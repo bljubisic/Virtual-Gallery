8d7e3b1e17ade9680f048dd748a13a09a9dc2f35
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2018-01-23 20:23:06 +0100

    Initial Commit

---
+++ Virtual Gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Gallery
+//
+//  Created by Bratislav Ljubisic on 1/23/18.
+//  Copyright © 2018 Bratislav Ljubisic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
