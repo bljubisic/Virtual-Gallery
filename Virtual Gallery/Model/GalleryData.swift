ae3fe0ebfd7e2f703e4ad7081df13d3f88c506f9
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2018-02-09 13:38:37 +0100

    Added fields and struct needed

--- Virtual Gallery/Model/GalleryData.swift
+++ Virtual Gallery/Model/GalleryData.swift
@@ -9,10 +9,94 @@
 import Foundation
 
 struct Criterium {
-    
+    let searchText: String
+    let  safeSearch: Bool
+}
+
+extension Criterium {
+    init() {
+        searchText = ""
+        safeSearch = false
+    }
 }
 
 struct Image {
-    
+    let name: String
+    let largeURL: String
+    let thumbURL: String
+    let description: String
+    let url: String
+    let year: Int
+    let exif: Exif
+    let author: Author
+    let comments: [String]
+    let platform: Platform
+}
+
+extension Image {
+    init() {
+        name        = ""
+        url         = ""
+        platform    = Platform()
+        largeURL    = ""
+        thumbURL    = ""
+        description = ""
+        year        = 0
+        exif        = Exif()
+        author      = Author()
+        comments    = [""]
+    }
+}
+
+struct Exif {
+    let exposure: String
+    let camera: String
+    let aperture: String
+    let licence: String
+    let iso: String
+    let focalLength: String
+}
+
+extension Exif {
+    init() {
+        exposure    = ""
+        camera      = ""
+        aperture    = ""
+        licence     = ""
+        iso         = ""
+        focalLength = ""
+    }
+}
+
+struct Author {
+    let username: String
+    let firstName: String
+    let lastName: String
+    let avatar: String
+    let url: String
+}
+
+extension Author {
+    init() {
+        username  = ""
+        firstName = ""
+        lastName  = ""
+        avatar    = ""
+        url       = ""
+    }
+}
+
+struct Platform {
+    let name: String
+    let homeURL: String
+    let haveAccount: Bool
+}
+
+extension Platform {
+    init() {
+        name = ""
+        homeURL = ""
+        haveAccount = false
+    }
 }
 
