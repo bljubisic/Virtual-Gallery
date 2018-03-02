531cfa2291ad12839e83397f56ebc121213b92d4
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2018-03-02 16:08:45 +0100

    Added init with Platform and needed methods for defining a gallery

--- Virtual Gallery/Model/GalleryProtocol.swift
+++ Virtual Gallery/Model/GalleryProtocol.swift
@@ -11,6 +11,14 @@ import RxSwift
 
 protocol GalleryModel: class {
     
-    func getImagesFor(_ criterium: Criterium) -> Variable<[Image]>;
+    init(WithPlatform platform: Platform)
+    
+    func connect(WithLogin login: Login) -> Bool
+    
+    func getImages(ForCriterium criterium: Criterium) -> Variable<[Image]>
+    
+    func getPlatform() -> Platform
+    
+    func isLoggedIn(WithLogin login: Login) -> Bool
     
 }
