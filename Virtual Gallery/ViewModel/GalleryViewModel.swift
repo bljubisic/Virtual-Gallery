a89c08060ec4fa6ed6e88e6377e28c5501c3f4a4
Bratislav Ljubisic <bratislav.ljubisic@gmail.com>
2018-03-23 21:44:54 +0100

    Added viewmodel for project

---
+++ Virtual Gallery/ViewModel/GalleryViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryViewModel.swift
+//  Virtual Gallery
+//
+//  Created by Bratislav Ljubisic on 3/23/18.
+//  Copyright © 2018 Bratislav Ljubisic. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class GalleryViewModel: GalleryViewModelProtocol {
+    func getImagesForCentralLine(forCriterium criteria: Criterium) -> Observable<[Image]> {
+        
+    }
+    
+    
+}
