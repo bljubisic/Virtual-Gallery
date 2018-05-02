4b172cf4fc7676e0936429c9130941d8feb60033
bljubisic <bratislav.ljubisic@gmail.com>
2018-05-02 12:31:57 +0200

    ViewModel is now subscriber to signals comming from Model. View does not know anything regarding those signals

--- Virtual Gallery/ViewModel/GalleryViewModel.swift
+++ Virtual Gallery/ViewModel/GalleryViewModel.swift
@@ -10,17 +10,54 @@ import Foundation
 import RxSwift
 
 class GalleryViewModel: GalleryViewModelProtocol {
+
     
     let model: GalleryModelProtocol
+    var centralLine: [Image] = [Image]()
+    var fuzzyImages: [Image] = [Image]()
+    var currentCentralImage: Image = Image()
+    
+    private let disposeBag: DisposeBag
     
     required init(withModel model: GalleryModelProtocol) {
         self.model = model
+        disposeBag = DisposeBag()
     }
     
-    func getImagesForCentralLine(forCriterium criteria: Criterium) -> Observable<[Image]> {
-        let centralLine = model.getCentralImages(WithLimit: 10).asObservable()
-        return centralLine
+    func getImagesForCentralLine(forCriterium criteria: Criterium, andCompletion completion:@escaping ([Image]) -> Void) -> Void{
+        self.centralLine = [Image] ()
+        var tmpCentralLine = [Image]()
+        
+        self.model.getCentralImages(WithLimit: 10).filter { (image) -> Bool in
+            return image.name != ""
+        }.map({[unowned self] (image) -> Image in
+            self.centralLine.append(image)
+            return image
+        }).take(3).subscribe( onNext: {result in
+            tmpCentralLine.append(result)
+        }, onError: { (Error) in
+            print("!!!!error")
+        }, onCompleted: {
+            print("!!!!completed")
+        }) {
+            if(tmpCentralLine.count >= 3) {
+                self.currentCentralImage = tmpCentralLine[1]
+                completion(tmpCentralLine)
+            }
+        }.disposed(by: disposeBag)
     }
     
-    
+    func downloadDataFor(ImageURL url: String, withCompletion completion: @escaping (Data) -> Void) {
+        self.model.downloadImageFrom(URL: url).filter { (data) -> Bool in
+            return !data.isEmpty
+        }.subscribe(onNext: { (data) in
+            completion(data)
+        }, onError: { (Error) in
+            
+        }, onCompleted: {
+            
+        }) {
+            
+            }.disposed(by: disposeBag)
+    }
 }
