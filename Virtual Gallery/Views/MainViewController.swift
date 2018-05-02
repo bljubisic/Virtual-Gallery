d25c93a6c6c5e54ac55f75736989fa5b95375015
bljubisic <bratislav.ljubisic@gmail.com>
2018-05-02 12:32:39 +0200

    View is only responsible for displaying images on screen

--- Virtual Gallery/Views/MainViewController.swift
+++ Virtual Gallery/Views/MainViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 import SnapKit
+import RxCocoa
+import RxSwift
 
 class MainViewController: UIViewController {
+    
+    var viewModel: GalleryViewModel!
     
     var topLeft: UIImageView!
     var topCenter: UIImageView!
@@ -20,11 +24,17 @@ class MainViewController: UIViewController {
     var lowLeft: UIImageView!
     var lowCenter: UIImageView!
     var lowRight: UIImageView!
+    
+    var disposableBag: DisposeBag!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        disposableBag = DisposeBag()
+        
+        self.view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         
         topLeft = UIImageView(frame: CGRect(x: 0, y: 0, width: 110, height: 110))
+        topLeft.backgroundColor = #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)
         self.view.addSubview(topLeft)
         
         topLeft.snp.makeConstraints { (make) in
@@ -33,6 +43,45 @@ class MainViewController: UIViewController {
             make.height.equalTo(110)
             make.width.equalTo(110)
         }
+        
+        midLeft = UIImageView(frame: CGRect(x: 0, y: 0, width: 110, height: 100))
+        topLeft.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        self.view.addSubview(midLeft)
+        
+        midLeft.snp.makeConstraints { make in
+            make.centerX.equalTo(self.view.snp.left)
+            make.centerY.equalTo(self.view.snp.centerY)
+            make.height.equalTo(110)
+            make.width.equalTo(110)
+        }
+        
+        lowLeft = UIImageView(frame: CGRect(x: 0, y: 0, width: 110, height: 110))
+        topLeft.backgroundColor = #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1)
+        self.view.addSubview(lowLeft)
+        
+        lowLeft.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.view.snp.left)
+            make.centerY.equalTo(self.view.snp.bottom)
+            make.height.equalTo(110)
+            make.width.equalTo(110)
+        }
+        
+        self.viewModel.getImagesForCentralLine(forCriterium: Criterium(), andCompletion: { images in
+            _ = images.map({ (image) -> Image in
+                print(image.name)
+                return image
+            })
+            self.viewModel.downloadDataFor(ImageURL: images[0].thumbURL, withCompletion: { (data) in
+                self.topLeft.image = UIImage(data: data)
+            })
+            self.viewModel.downloadDataFor(ImageURL: images[1].thumbURL, withCompletion: { (data) in
+                self.midLeft.image = UIImage(data: data)
+            })
+            self.viewModel.downloadDataFor(ImageURL: images[2].thumbURL, withCompletion: { (data) in
+                self.lowLeft.image = UIImage(data: data)
+            })
+        })
+        
 
         // Do any additional setup after loading the view.
     }
